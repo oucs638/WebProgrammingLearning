8bec4a761aaea4ccf3704cdbff7436ea91622d54
oucs638 <oucs638@gmail.com>
2019-01-21 20:16:22 +0800

    change dev

---
+++ NodeJS/hello.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer((req, res) => {
+  var name = require('url').parse(req.url, true).query.name;
+
+  if (name == undefined) { name = 'World'; }
+
+  if (name == 'burningbird')
+  {
+    var file = 'phoenix5a.png';
+    fs.stat(file, (err, stat) => {
+      if (err)
+      {
+        console.log(err);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("Sorry, Burningbird isn't around right now \n");
+      }
+      else
+      {
+        var img = fs.readFileSync(file);
+        res.contentType = 'image/png';
+        res.contentLength = stat.size;
+        res.end(img, 'binary');
+      }
+    });
+  }
+  else
+  {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello ' + name + '\n');
+  }
+}).listen(8124);
+
+console.log('Sever running at port 8124');
